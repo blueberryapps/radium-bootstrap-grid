fa3738f4b1abebcaefe0c90c7a77de3a51a6b79d
Jiří Orság <georg77@gmail.com>
2016-04-17 18:06:52 +0200

    New features

--- src/column.react.js
+++ src/column.react.js
@@ -1,7 +1,7 @@
 import Component from 'react-pure-render/component';
 import Radium from 'radium';
 import React, {PropTypes as RPT} from 'react';
-import { column, columnHidden, columnWidth } from './grid';
+import { column, columnHidden, columnOffset, columnPull, columnPush, columnWidth } from './grid';
 
 @Radium
 export default class Column extends Component {
@@ -10,19 +10,63 @@ export default class Column extends Component {
     children: RPT.node,
     lg:       RPT.number,
     lgHidden: RPT.bool,
+    lgOffset: RPT.number,
+    lgPull:   RPT.number,
+    lgPush:   RPT.number,
     md:       RPT.number,
     mdHidden: RPT.bool,
+    mdOffset: RPT.number,
+    mdPull:   RPT.number,
+    mdPush:   RPT.number,
     ms:       RPT.number,
     msHidden: RPT.bool,
+    msOffset: RPT.number,
+    msPull:   RPT.number,
+    msPush:   RPT.number,
     sm:       RPT.number,
     smHidden: RPT.bool,
+    smOffset: RPT.number,
+    smPull:   RPT.number,
+    smPush:   RPT.number,
     style:    RPT.object,
     xs:       RPT.number,
-    xsHidden: RPT.bool
+    xsHidden: RPT.bool,
+    xsOffset: RPT.number,
+    xsPull:   RPT.number,
+    xsPush:   RPT.number
   }
 
   render() {
-    const { children, lg, lgHidden, md, mdHidden, ms, msHidden, sm, smHidden, style, xs, xsHidden, ...props } = this.props;
+    const {
+      children,
+      lg,
+      lgHidden,
+      lgOffset,
+      lgPull,
+      lgPush,
+      md,
+      mdHidden,
+      mdOffset,
+      mdPull,
+      mdPush,
+      ms,
+      msHidden,
+      msOffset,
+      msPull,
+      msPush,
+      sm,
+      smHidden,
+      smOffset,
+      smPull,
+      smPush,
+      style,
+      xs,
+      xsHidden,
+      xsOffset,
+      xsPull,
+      xsPush,
+      ...props
+    } = this.props;
 
     return (
       <div
@@ -30,15 +74,30 @@ export default class Column extends Component {
           column,
           lg && columnWidth['lg'][lg],
           lgHidden && columnHidden['lg'],
+          lgOffset && columnOffset['lg'][lgOffset],
+          lgPull && columnPull['lg'][lgPull],
+          lgPush && columnPush['lg'][lgPush],
           md && columnWidth['md'][md],
           mdHidden && columnHidden['md'],
+          mdOffset && columnOffset['md'][mdOffset],
+          mdPull && columnPull['md'][mdPull],
+          mdPush && columnPush['md'][mdPush],
           ms && columnWidth['ms'][ms],
           msHidden && columnHidden['ms'],
+          msOffset && columnOffset['ms'][msOffset],
+          msPull && columnPull['ms'][msPull],
+          msPush && columnPush['ms'][msPush],
           sm && columnWidth['sm'][sm],
           smHidden && columnHidden['sm'],
-          style,
           xs && columnWidth['xs'][xs],
-          xsHidden && columnHidden['xs']
+          smOffset && columnOffset['sm'][smOffset],
+          smPull && columnPull['sm'][smPull],
+          smPush && columnPush['sm'][smPush],
+          xsHidden && columnHidden['xs'],
+          xsOffset && columnOffset['xs'][xsOffset],
+          xsPull && columnPull['xs'][xsPull],
+          xsPush && columnPush['xs'][xsPush],
+          style
         ]}
         {...props}
       >
